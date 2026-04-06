8d850fd899cc045021742b573622ba759d8629c8
Nida Rahma <nida303@gmail.com>
2026-04-06 15:12:59 +0700

    update javascript operasi

---
+++ DAY3/belajar_javascript/latihan08.js
@@ -0,0 +1,16 @@
+var panjang = "12";
+var lebar = "7";
+var alas = '6';
+var tinggi = '13';
+
+var angka = parseInt(panjang);
+var angka2 = parseInt(lebar);
+var angka3 = parseInt(alas);
+var angka4 = parseInt(tinggi);
+
+let luasPersegiPanjang = angka * angka2;
+console.log("Luas Persegi Panjang : " + luasPersegiPanjang);
+
+
+let luasSegitiga = 0.5 * angka3 * angka4;
+console.log("Luas Segitiga : " + luasSegitiga);
