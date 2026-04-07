a977abdc1823f30b540a24871ddc727381a79dbc
Nida Rahma <nida303@gmail.com>
2026-04-07 08:34:48 +0700

    update day4

---
+++ DAY4/latihan08.js
@@ -0,0 +1,8 @@
+var sisi = " 1 2 ";
+var jariJari = "7.5";
+
+sisi = Number(sisi.replaceAll(" ", ""));
+jariJari = parseFloat(jariJari);
+
+
+console.log("luas persegi : " + 3.14 * jariJari * jariJari);
