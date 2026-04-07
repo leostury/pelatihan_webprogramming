4ff72f7d55c41a2f8c07e387f7d18ebaca8d1d69
Nida Rahma <nida303@gmail.com>
2026-04-07 15:12:00 +0700

    update function

---
+++ DAY4/latihan24.js
@@ -0,0 +1,28 @@
+
+
+// function siji() {
+//   const angka = [2, 3, 1, 9, 12, 8, 9, 7];
+
+//   let terbesar = angka[0];
+//   let terkecil = angka[0];
+
+//   for (let i = 0; i < angka.length; i++) {
+//     if (angka[i] > terbesar) {
+//       terbesar = angka[i];
+//     }
+
+//     if (angka[i] < terkecil) {
+//       terkecil = angka[i];
+//     }
+//   }
+
+//   return 'angka terbesar adalah ${terbesar} dan angka terkecil adalah ${terkecil}';
+// }
+
+// console.log(siji());
+
+
+const angka = [2, 3, 1, 9, 12, 8, 9, 7];
+const angkaMaxMin(array){
+    const max = Math.max(...array);
+}
---
+++ DAY4/latihan25.js
@@ -0,0 +1,7 @@
+function arrangeString (str){
+    return str.split("").sort().join("");
+}
+
+console.log(arrangeString("bahasa"));
+console.log(arrangeString("BBPVP"));
+console.log(arrangeString("kompeten"))
